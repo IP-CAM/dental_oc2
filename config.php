1ad832171e6c4985f33929b30040ce82f33f61c9
Ali Abbas <itsgeniusstar@gmail.com>
2014-12-13 14:04:22 +0500

    add just one time more

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/brian/real_dental/Dental/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/brian/real_dental/Dental/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/brian/real_dental/Dental/catalog/');
+define('DIR_SYSTEM', '/var/www/html/brian/real_dental/Dental/system/');
+define('DIR_DATABASE', '/var/www/html/brian/real_dental/Dental/system/database/');
+define('DIR_LANGUAGE', '/var/www/html/brian/real_dental/Dental/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/brian/real_dental/Dental/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/brian/real_dental/Dental/system/config/');
+define('DIR_IMAGE', '/var/www/html/brian/real_dental/Dental/image/');
+define('DIR_CACHE', '/var/www/html/brian/real_dental/Dental/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/brian/real_dental/Dental/download/');
+define('DIR_LOGS', '/var/www/html/brian/real_dental/Dental/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'dental2');
+define('DB_PREFIX', 'oc_');
+?>
