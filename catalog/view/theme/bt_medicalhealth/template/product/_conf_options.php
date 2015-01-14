ac5d64b94175efb165f8103f434be2c214f1ad30
Ali <ali.abbas@nxb.com.pk>
2015-01-14 20:28:47 +0500

    test

--- catalog/view/theme/bt_medicalhealth/template/product/_conf_options.php
+++ catalog/view/theme/bt_medicalhealth/template/product/_conf_options.php
@@ -5,7 +5,7 @@ echo "<b>Arcade</b><br /><br />";
 <div id="option-arcade" class="option">
     <?php foreach ($product_config_options_arc as $option_v) { ?>
         <span style="margin-right:10px;">
-            <input type="radio" name="option[<?php echo $keyconf ?>]" value="<?php echo $option_v['id']; ?>" id="option-value-<?php echo $option_v['value']; ?>" />
+            <input type="radio" name="option[arcade]" value="<?php echo $option_v['id']; ?>" id="option-value-<?php echo $option_v['value']; ?>" />
             <label for="option-value-<?php echo $option_v['id']; ?>"><?php echo $option_v['value']; ?>
 
             </label>
@@ -17,17 +17,24 @@ echo "<b>Arcade</b><br /><br />";
         foreach ($product_config_options_arc as $option_v) {
         $json_data = $option_v['json_data'];
         $json_data = json_decode($json_data,true);
-            
+
+        if(isset($json_data['toms']) && is_array($json_data['toms'])){
+            foreach($json_data['toms'] as $tom){
+
      ?>
         <span style="margin-right:10px;">
-            <input type="radio" name="option[<?php echo $keyconf ?>]" value="<?php echo $option_v['id']; ?>" id="option-value-<?php echo $option_v['value']; ?>" />
-            <label for="option-value-<?php echo $option_v['id']; ?>"><?php echo $option_v['value']; ?>
+            <input type="radio" name="option[tom]" value="<?php echo $tom['tom']; ?>" id="option-value-<?php echo $tom['tom']; ?>" />
+            <label for="option-value-<?php echo $tom['tom']; ?>"><?php echo $tom['tom']; ?>
 
             </label>
         </span>
+    <?php
+            }
+        }
+    ?>    
     <?php } ?>
 </div>
 <br />
 <?php
-
+die;
 ?>
