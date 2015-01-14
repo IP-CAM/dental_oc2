9316ae579bb9aba428adfe9195145e8b097dce7a
Ali <ali.abbas@nxb.com.pk>
2015-01-14 16:14:41 +0500

    test commit

--- catalog/view/theme/bt_medicalhealth/template/product/_conf_options.php
+++ catalog/view/theme/bt_medicalhealth/template/product/_conf_options.php
@@ -1,23 +1,33 @@
 <?php
-foreach ($product_config_options as $keyconf => $conf) {
+$product_config_options_arc = $product_config_options['arcade'];
+echo "<b>Arcade</b><br /><br />";
+?>
+<div id="option-arcade" class="option">
+    <?php foreach ($product_config_options_arc as $option_v) { ?>
+        <span style="margin-right:10px;">
+            <input type="radio" name="option[<?php echo $keyconf ?>]" value="<?php echo $option_v['id']; ?>" id="option-value-<?php echo $option_v['value']; ?>" />
+            <label for="option-value-<?php echo $option_v['id']; ?>"><?php echo $option_v['value']; ?>
 
-    echo "<b>" . ucfirst($keyconf) . "</b><br /><br />";
-    ?>
-    <div id="option-arcade" class="option">
+            </label>
+        </span>
+    <?php } ?>
+</div>
+<div id="option-arcade" class="option">
+     <?php 
+        foreach ($product_config_options_arc as $option_v) {
+        $json_data = $option_v['json_data'];
+        $json_data = json_decode($json_data,true);
+            
+     ?>
+        <span style="margin-right:10px;">
+            <input type="radio" name="option[<?php echo $keyconf ?>]" value="<?php echo $option_v['id']; ?>" id="option-value-<?php echo $option_v['value']; ?>" />
+            <label for="option-value-<?php echo $option_v['id']; ?>"><?php echo $option_v['value']; ?>
 
+            </label>
+        </span>
+    <?php } ?>
+</div>
+<br />
+<?php
 
-        <?php foreach ($conf as $option_v) { ?>
-            <span style="margin-right:10px;">
-                <input type="radio" name="option[<?php echo $keyconf ?>]" value="<?php echo $option_v['id']; ?>" id="option-value-<?php echo $option_v['value']; ?>" />
-                <label for="option-value-<?php echo $option_v['id']; ?>"><?php echo $option_v['value']; ?>
-
-                </label>
-            </span>
-
-
-        <?php } ?>
-    </div>
-    <br />
-    <?php
-}
 ?>
