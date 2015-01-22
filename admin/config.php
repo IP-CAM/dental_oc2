f470c7f46f1ef1d07e8c376083d2e63f0daa4e04
Ali Abbas <itsgeniusstar@gmail.com>
2015-01-23 00:48:45 +0500

    installation of columns

--- admin/config.php
+++ admin/config.php
@@ -9,16 +9,16 @@ define('HTTPS_CATALOG', 'http://localhost/brian/real_dental/Dental/');
 
 // DIR
 define('DIR_APPLICATION', '/var/www/html/brian/real_dental/Dental/admin/');
-define('DIR_SYSTEM', '//var/www/html/brian/real_dental/Dental/system/');
-define('DIR_DATABASE', '//var/www/html/brian/real_dental/Dental/system/database/');
-define('DIR_LANGUAGE', '//var/www/html/brian/real_dental/Dental/admin/language/');
-define('DIR_TEMPLATE', '//var/www/html/brian/real_dental/Dental/admin/view/template/');
-define('DIR_CONFIG', '//var/www/html/brian/real_dental/Dental/system/config/');
-define('DIR_IMAGE', '//var/www/html/brian/real_dental/Dental/image/');
-define('DIR_CACHE', '//var/www/html/brian/real_dental/Dental/system/cache/');
-define('DIR_DOWNLOAD', '//var/www/html/brian/real_dental/Dental/download/');
-define('DIR_LOGS', '//var/www/html/brian/real_dental/Dental/system/logs/');
-define('DIR_CATALOG', '//var/www/html/brian/real_dental/Dental/catalog/');
+define('DIR_SYSTEM', '/var/www/html/brian/real_dental/Dental/system/');
+define('DIR_DATABASE', '/var/www/html/brian/real_dental/Dental/system/database/');
+define('DIR_LANGUAGE', '/var/www/html/brian/real_dental/Dental/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/brian/real_dental/Dental/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/brian/real_dental/Dental/system/config/');
+define('DIR_IMAGE', '/var/www/html/brian/real_dental/Dental/image/');
+define('DIR_CACHE', '/var/www/html/brian/real_dental/Dental/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/brian/real_dental/Dental/download/');
+define('DIR_LOGS', '/var/www/html/brian/real_dental/Dental/system/logs/');
+define('DIR_CATALOG', '/var/www/html/brian/real_dental/Dental/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
