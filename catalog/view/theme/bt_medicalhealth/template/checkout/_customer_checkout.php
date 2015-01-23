2c361b68bb62a55480111ef90a96c4936ada853e
Ali Abbas <itsgeniusstar@gmail.com>
2015-01-24 00:07:03 +0500

    installation of columns

--- catalog/view/theme/bt_medicalhealth/template/checkout/_customer_checkout.php
+++ catalog/view/theme/bt_medicalhealth/template/checkout/_customer_checkout.php
@@ -12,13 +12,13 @@
             Nome Completo
         </td>
         <td>
-            <input type="text" />
+            <input type="text" name="payment_cad_name" id="payment_cad_name" />
         </td>
         <td>
             Data de Nascimento
         </td>
         <td>
-            <input type="text" />
+            <input type="text" name="payment_cad_dob" id="payment_cad_dob" />
         </td>
     </tr>
     <tr>
@@ -27,13 +27,13 @@
             CPF
         </td>
         <td>
-            <input type="text" />
+            <input type="text" name="payment_cad_cpf" id="payment_cad_cpf" />
         </td>
         <td>
             RG
         </td>
         <td>
-            <input type="text" />
+            <input type="text" name="payment_cad_rg" id="payment_cad_rg" />
         </td>
     </tr>
     <tr>
@@ -42,13 +42,13 @@
             Telefone
         </td>
         <td>
-            <input type="text" />
+            <input type="text" name="payment_cad_telefone" id="payment_cad_telefone" />
         </td>
         <td>
             Celular
         </td>
         <td>
-            <input type="text" />
+            <input type="text" name="payment_cad_celular" id="payment_cad_celular" />
         </td>
     </tr>
     <tr>
@@ -77,13 +77,13 @@
             Razão Social
         </td>
         <td>
-            <input type="text" />
+            <input type="text" name="payment_corop_name" id="payment_corop_name" />
         </td>
         <td>
-           Nome Fantasia
+            Nome Fantasia
         </td>
         <td>
-            <input type="text" />
+            <input type="text" name="payment_corop_trade_name" id="payment_corop_trade_name" />
         </td>
     </tr>
     <tr>
@@ -92,13 +92,13 @@
             CNPJ
         </td>
         <td>
-            <input type="text" />
+            <input type="text" name="payment_corop_cnpg" id="payment_corop_cnpg"/>
         </td>
         <td>
             Nome do Responsável
         </td>
         <td>
-            <input type="text" />
+            <input type="text" id="payment_corop_responsible_name" name="payment_corop_responsible_name" />
         </td>
     </tr>
     <tr>
@@ -107,29 +107,138 @@
             Telefone
         </td>
         <td>
-            <input type="text" />
+            <input type="text" name="payment_corop_telefone" id="payment_corop_telefone"/>
         </td>
         <td>
             Celular do Responsável
         </td>
         <td>
-            <input type="text" />
+            <input type="text" name="payment_corop_responsible_cell" id="payment_corop_responsible_cell" />
         </td>
     </tr>
-      <tr>
+    <tr>
 
         <td>
             Inscrição Estadual
         </td>
         <td>
-            <input type="text" />
+            <input type="text" name="payment_corop_state_registration" id="payment_corop_state_registration" />
         </td>
         <td>
             Isento
         </td>
         <td>
-            <input type="checkbox" />
+            <input type="checkbox" name="payment_corop_isento" id="payment_corop_isento" />
         </td>
     </tr>
-   
+
+</table>
+
+<table>
+    <tr>
+
+        <th colspan="4">Dados Profissionais</th>
+    </tr>
+    <tr>
+
+        <td colspan="4">
+            <label>Odontologista</label>
+            <input type="radio" name="payment_profession_type" value="Odontologista" />
+            <label>Protético</label>
+            <input type="radio" name="payment_profession_type" value="Protético" />
+            <label>Acadêmico</label>
+            <input type="radio" name="payment_profession_type" value="Acadêmico" />
+        </td>
+    </tr>
+    <tr>
+
+        <td>
+            CRO
+        </td>
+        <td colspan="3"> 
+            <input type="text" id="payment_profession_cro" name="payment_profession_cro" />
+        </td>
+
+    </tr>
+    <tr>
+
+        <td>
+            TDP
+        </td>
+        <td colspan="3"> 
+            <input type="text" id="payment_profession_tdp" name="payment_profession_tdp" />
+        </td>
+
+    </tr>
+    <tr>
+        <td>
+            Número da Matrícula
+        </td>
+        <td colspan="3">
+            <input type="text" id="payment_profession_matricula" name="payment_profession_matricula" />
+        </td>
+
+    </tr>
+    <tr>
+        <td>
+            Instituição de Ensino
+        <td colspan="3">
+            <input type="text" id="payment_profession_ensino" name="payment_profession_ensino" />
+        </td>
+    </tr>
+    <tr>
+
+        <td>
+            Graduação
+        </td>
+        <td>
+            <input type="text" id="payment_profession_graduacao" name="payment_profession_graduacao" />
+        </td>
+        <td>
+            Instituição de Ensino
+        </td>
+        <td>
+            <?php
+            $prof_inst = array(
+                'Selecione',
+                'Superior Completo',
+                'Pós Graduação',
+                'Mestrado',
+                'Doutorado',
+            );
+            ?>
+            <select id="payment_profession_instituica" name="payment_profession_instituica">
+                <?php
+                foreach ($prof_inst as $val) {
+                    echo "<option value='" . $val . "'>" . $val . "</option>";
+                }
+                ?>
+            </select>
+        </td>
+    </tr>
+    <tr>
+
+        <td>
+            Área de Atuação
+        </td>
+        <td colspan="3">
+            <?php
+            $options_atuaca = array(
+                "Clínica", "Dentística",
+                "Endodontia", "Estética",
+                "Ortodondia", "Periodontia",
+                "Prótese", "Radiologia",
+            );
+            foreach ($options_atuaca as $opt) {
+                ?>
+                <label><?php echo $opt; ?></label>    
+                <input type="checkbox" name="payment_profession_atuacao" value="<?php echo $opt; ?>" />
+                <?php
+            }
+            ?>
+
+        </td>
+
+    </tr>
+
 </table>
