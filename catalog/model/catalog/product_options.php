5823772d03f036872a7ccf90b92d0008c5a33874
Ali Abbas <itsgeniusstar@gmail.com>
2015-03-18 23:51:53 +0500

    Options frontend with javascript

--- catalog/model/catalog/product_options.php
+++ catalog/model/catalog/product_options.php
@@ -1,35 +1,36 @@
 <?php
 
 class ModelCatalogProductOptions extends Model {
+
     /**
      * 
      */
-    public function gerProductOptions($product_id, $option_type = 'arcade',$options_arr = array()){
+    public function gerProductOptions($product_id, $option_type = 'arcade', $options_arr = array()) {
         $options_types = array(
-            'arcade'=>'arcade',
-            'cor'=>'cor',
-            'quantitdy'=>'quantity',
-            'tamanho'=>'tamanho',
+            'arcade' => 'arcade',
+            'tamanho' => 'tamanho',
+            'quantitdy' => 'quantity',
+            'cor' => 'cor',
         );
-        
-        $sub_query = "SELECT product_id FROM ".DB_PREFIX."product WHERE referenc_id =".$product_id;
+
+        $sub_query = "SELECT product_id FROM " . DB_PREFIX . "product WHERE referenc_id =" . $product_id;
         $column = "value";
-        if($this->config->get('config_language')!="en"){
-            $column = "value_".$this->config->get('config_language');
+        if ($this->config->get('config_language') != "en") {
+            $column = "value_" . $this->config->get('config_language');
         }
-        $sql = "Select DISTINCT($option_type) as option_id,$column as value FROM ".DB_PREFIX."product_config_options t INNER JOIN ".
-            " ".DB_PREFIX."conf_product_".$options_types[$option_type]." op ON op.id = t.".$option_type." WHERE t.product_id IN($sub_query) ";
+        $sql = "Select DISTINCT($option_type) as option_id,$column as value FROM " . DB_PREFIX . "product_config_options t INNER JOIN " .
+                " " . DB_PREFIX . "conf_product_" . $options_types[$option_type] . " op ON op.id = t." . $option_type . " WHERE t.product_id IN($sub_query) ";
         $sql.= ' ';
-        
-       
+
+
         $where = '';
-        if(!empty($options_arr)){
-             $where = array();
-            foreach($options_arr as $key=>$option){
-              $where[] = "t.".$key.' = '.$option;
+        if (!empty($options_arr)) {
+            $where = array();
+            foreach ($options_arr as $key => $option) {
+                $where[] = "t." . $key . ' = ' . $option;
             }
-            if(!empty($where)){
-                $where = "AND ".implode(" AND ", $where);
+            if (!empty($where)) {
+                $where = "AND " . implode(" AND ", $where);
             }
         }
         $sql.= $where;
@@ -38,6 +39,17 @@ class ModelCatalogProductOptions extends Model {
         $query = $this->db->query($sql);
         return $query->rows;
     }
+
+    public function getOptionCount($product_id, $column) {
+        $sub_query = "SELECT product_id FROM " . DB_PREFIX . "product WHERE referenc_id =" . $product_id;
+
+        $sql = "Select count($column) as count FROM " . DB_PREFIX . "product_config_options t  " .
+                " " . " WHERE t.product_id IN($sub_query) AND t.$column IS NOT NULL AND t.$column<>'' ";
+        $sql.= ' ';
+
+        return $this->db->query($sql)->row['count'];
+    }
+
 }
 
 ?>
--- catalog/view/theme/bt_medicalhealth/template/product/_conf_options.php
+++ catalog/view/theme/bt_medicalhealth/template/product/_conf_options.php
@@ -1,51 +1,152 @@
 
 <script>
-var arcades = [];
-var all_options = <?php echo json_encode($product_config_all);
+    var arcades = [];
+    var all_options = <?php echo json_encode($product_config_all);
 ?>;
 </script>
 <?php
 $this->load->model('catalog/product_options');
 $options_arcade = $this->model_catalog_product_options->gerProductOptions($product_id);
 
+$arcade_count = $this->model_catalog_product_options->getOptionCount($product_id, 'arcade');
+$tamanho_count = $this->model_catalog_product_options->getOptionCount($product_id, 'tamanho');
+$quantitdy_count = $this->model_catalog_product_options->getOptionCount($product_id, 'quantitdy');
+$cor_count = $this->model_catalog_product_options->getOptionCount($product_id, 'cor');
 
 
-echo "<b>Arcade</b><br /><br />";
-?>
-<div id="option-arcade" class="option">
-    <?php
-    $index = 0;
-    foreach ($options_arcade as $option_v) {
-        ?>
-        <span style="margin-right:10px;">
-            <input db_id="<?php echo $option_v['option_id']; ?>" index="<?php echo $index ?>" type="radio" name="option[arcade]" 
-                   value="<?php echo $option_v['option_id']; ?>" 
-                   id="option-value-<?php echo $option_v['option_id']; ?>" />
-            <label for="option-value-<?php echo $option_v['option_id']; ?>"><?php echo $option_v['value']; ?>
+$level_tree = array();
+if (!empty($options_arcade)) {
+    $level_tree[] = 'arcade';
+}
 
-            </label>
-        </span>
-        <?php
-        $index++;
-    }
+if ($arcade_count > 0) {
+    echo "<b>Arcade</b><br /><br />";
     ?>
-</div>
-<br/>
+    <div id="option-arcade" class="option">
+        <?php
+        $index = 0;
+        foreach ($options_arcade as $option_v) {
+            ?>
+            <span style="margin-right:10px;">
+                <input db_id="<?php echo $option_v['option_id']; ?>" index="<?php echo $index ?>" type="radio" name="option[arcade]" 
+                       value="<?php echo $option_v['option_id']; ?>" 
+                       id="option-value-<?php echo $option_v['option_id']; ?>" />
+                <label for="option-value-<?php echo $option_v['option_id']; ?>"><?php echo $option_v['value']; ?>
 
-<?php
-echo "<b>Tamanho</b><br /><br />";
+                </label>
+            </span>
+            <?php
+            $index++;
+        }
+        ?>
+    </div>
+    <br/>
+    <?php
+}
 ?>
-<div id="option-tom" class="option">
+<?php
+if ($tamanho_count > 0) {
+    echo "<b>Tamanho</b><br /><br />";
+    ?>
+    <div id="option-tom" class="option">
+        <?php
+        if ($arcade_count == 0) {
+            $options_tamanho = $this->model_catalog_product_options->gerProductOptions($product_id, 'tamanho');
 
-</div>
+            if (!empty($options_tamanho)) {
+                $level_tree[] = 'tamanho';
+
+                $index = 0;
+                foreach ($options_tamanho as $option_v) {
+                    ?>
+                    <span style="margin-right:10px;">
+                        <input db_id="<?php echo $option_v['option_id']; ?>" index="<?php echo $index ?>" type="radio" name="option[tamanho]" 
+                               value="<?php echo $option_v['option_id']; ?>" 
+                               id="option-value-<?php echo $option_v['option_id']; ?>" />
+                        <label for="option-value-<?php echo $option_v['option_id']; ?>"><?php echo $option_v['value']; ?>
+
+                        </label>
+                    </span>
+                    <?php
+                    $index++;
+                }
+            }
+        }
+        ?>
+    </div>
+    <?php
+}
+?>
 <br/>
 <?php
-echo "<b>Cor</b><br /><br />";
+if ($quantitdy_count > 0) {
+    echo "<b>Quantitdy</b><br /><br />";
+    ?>
+
+    <div id="option-quantitdy" class="option">
+        <?php
+        if ($arcade_count == 0 && $tamanho_count == 0) {
+            $options_quantitdy = $this->model_catalog_product_options->gerProductOptions($product_id, 'quantitdy');
+            if (!empty($options_quantitdy)) {
+
+                $level_tree[] = 'quantitdy';
+
+                $index = 0;
+                foreach ($options_quantitdy as $option_v) {
+                    ?>
+                    <span style="margin-right:10px;">
+                        <input db_id="<?php echo $option_v['option_id']; ?>" index="<?php echo $index ?>" type="radio" name="option[quantitdy]" 
+                               value="<?php echo $option_v['option_id']; ?>" 
+                               id="option-value-<?php echo $option_v['option_id']; ?>" />
+                        <label for="option-value-<?php echo $option_v['option_id']; ?>"><?php echo $option_v['value']; ?>
+
+                        </label>
+                    </span>
+                    <?php
+                    $index++;
+                }
+            }
+        }
+        ?>
+    </div>
+    <?php
+}
 ?>
+<br/>
+<?php
+if ($cor_count > 0) {
+    echo "<b>Cor</b><br /><br />";
+    ?>
 
-<div id="option-cor" class="option">
+    <div id="option-cor" class="option">
+        <?php
+        if ($arcade_count == 0 && $tamanho_count == 0 && $quantitdy_count == 0) {
+            $options_cor = $this->model_catalog_product_options->gerProductOptions($product_id, 'cor');
+            if (!empty($options_cor)) {
 
-</div>
+                $level_tree[] = 'cor';
+
+                $index = 0;
+                foreach ($options_cor as $option_v) {
+                    ?>
+                    <span style="margin-right:10px;">
+                        <input db_id="<?php echo $option_v['option_id']; ?>" index="<?php echo $index ?>" type="radio" name="option[cor]" 
+                               value="<?php echo $option_v['option_id']; ?>" 
+                               id="option-value-<?php echo $option_v['option_id']; ?>" />
+                        <label for="option-value-<?php echo $option_v['option_id']; ?>"><?php echo $option_v['value']; ?>
+
+                        </label>
+                    </span>
+                    <?php
+                    $index++;
+                }
+            }
+        }
+        ?>
+    </div>
+    <?php
+}
+?>
 <input type="hidden" id="conf_option_id" name="option[conf_id]" value="" />
 
 <script>
@@ -60,17 +161,26 @@ echo "<b>Cor</b><br /><br />";
 
         })
         $("#option-tom input").live('click', function() {
-            
-            
+
+
             url = "?route=product/conf_product/options&product_id=<?php echo $product_id; ?>";
-            url += "&option_key=cor&option[tamanho]=" + $(this).val();
+            url += "&option_key=quantitdy&option[tamanho]=" + $(this).val();
             $.getJSON(url, function(data) {
-                console.log(data);
+                renderQuantity(data);
+            });
+    
+        })
+        $("#option-quantitdy input").live('click', function() {
+
+
+            url = "?route=product/conf_product/options&product_id=<?php echo $product_id; ?>";
+            url += "&option_key=cor&option[quantitdy]=" + $(this).val();
+            $.getJSON(url, function(data) {
                 renderCor(data);
             });
-
-//            
+    
         })
+
     })
 
     function renderToms(toms) {
@@ -91,10 +201,28 @@ echo "<b>Cor</b><br /><br />";
         $("#option-tom").html(htm);
     }
 
-    function renderCor(cors, tom_value) {
+    function renderQuantity(quantaties) {
 
         htm = "";
-        disabled ='';
+        disabled = '';
+
+        $.each(quantaties, function(k, v) {
+            htm += '<span style="margin-right:10px;">' +
+                    '<input  type="radio" name="option[quantitdy]"' +
+                    'value="' + v['option_id'] + '" ' +
+                    'id="option-value-' + v['option_id'] + '" ' + disabled + ' ' + v['value'] + ' />' +
+                    '<label for="option-value-' + v['option_id'] + '">' + v['value'] +
+                    '</label>' +
+                    '</span>';
+        })
+
+        $("#option-quantitdy").html(htm);
+
+    }
+    function renderCor(cors) {
+
+        htm = "";
+        disabled = '';
 
         $.each(cors, function(k, v) {
             htm += '<span style="margin-right:10px;">' +
